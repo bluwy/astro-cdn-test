8b20abbdec3b59b7bc8374c8787d7d37074658ef
bluwy <bjornlu.dev@gmail.com>
2023-04-03 21:50:41 +0800

    Set assetsPrefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import sitemap from '@astrojs/sitemap'
 export default defineConfig({
   site: 'https://bluwy.github.io',
   base: '/astro-cdn-test',
+  build: {
+    assetsPrefix: 'https://astro-cdn-test.pages.dev'
+  },
   integrations: [mdx(), sitemap()],
   experimental: {
     assets: true
